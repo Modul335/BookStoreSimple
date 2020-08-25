98d48e77b130b908cf6aec70020a4c5b0f79a0a9
unknown <m335@negwer.dev>
2020-08-25 14:01:39 +0200

    Initial commit JN1

---
+++ app/src/main/java/com/example/bookstoresimple/AddActivity.java
@@ -0,0 +1,96 @@
+package com.example.bookstoresimple;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class AddActivity extends AppCompatActivity {
+
+    private Button buttonSave = null;
+    private Button buttonReset = null;
+
+    private EditText titelText = null;
+    private EditText autorText = null;
+    private EditText verlagText = null;
+    private TextView isbnText = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+
+        final Button buttomHome = (Button) findViewById(R.id.buttonHome);
+        buttomHome.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                buttonHomeClicked();
+            }
+        });
+        final Button buttomAdd = (Button) findViewById(R.id.buttonAdd);
+        buttomAdd.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                buttonAddClicked();
+            }
+        });
+        final Button buttomHList = (Button) findViewById(R.id.buttonList);
+        buttomHList.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                buttonListClicked();
+            }
+        });
+
+        buttonSave = (Button) findViewById(R.id.buttonSave);
+        buttonSave.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("doButtonSaveClick");
+                doButtonSaveClick();
+            }
+        });
+
+        buttonReset = (Button) findViewById(R.id.buttonReset);
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                System.out.println("doButtonResetClick");
+                doButtonResetClick();
+            }
+        });
+
+        autorText = (EditText)findViewById(R.id.bookTitel);
+        titelText = (EditText) findViewById(R.id.bookAutor);
+        verlagText = (EditText) findViewById(R.id.bookVerlag);
+        isbnText = (EditText) findViewById(R.id.bookIsbn);
+
+        Intent intent = getIntent();
+        if( intent != null )  {
+            String message = intent.getStringExtra( "message" );
+            TextView lParam = (TextView) findViewById(R.id.listParam);
+            lParam.setText(message);
+        }
+    }
+
+
+    private void buttonHomeClicked() {
+        Intent intent = new Intent(this, MainActivity.class);
+        String message = "Come from Add!";
+        intent.putExtra("message", message);
+        startActivity(intent);
+    }
+
+    private void buttonAddClicked() {
+        Intent intent = new Intent(this, AddActivity.class);
+        String message = "Come from Add!";
+        intent.putExtra("message", message);
+        startActivity(intent);
+    }
+
+    private void buttonListClicked() {
+        Intent intent = new Intent(this, ListActivity.class);
+        String message = "Come from Add!";
+        intent.putExtra("message", message);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/example/bookstoresimple/model/Book.java
@@ -0,0 +1,23 @@
+package com.example.bookstoresimple.model;
+
+
+import com.orm.SugarRecord;
+
+public class Book extends SugarRecord {
+    String title;
+    String autor;
+    String verlag;
+    String isbn;
+
+
+    public Book(){
+    }
+
+    public Book(String title, String autor, String verlag, String isbn){
+        this.title = title;
+        this.autor = autor;
+        this.verlag = verlag;
+        this.isbn = isbn;
+    }
+}
+
